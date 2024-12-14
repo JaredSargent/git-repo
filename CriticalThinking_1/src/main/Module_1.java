9a1c5afdc8b99429623872af3f3d7804e97c0412
JaredSargent <jared.sargent@csuglobal.edu>
2024-12-14 17:20:07 -0500

    CriticalThinking

---
+++ CriticalThinking_1/src/main/Module_1.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Module_1 {
+
+	public static void main(String[] args) {
+		String firstName = "Harry";
+		String lastName = "Potter";
+		String streetAdd = "4 Privet Drive";
+		String zipCode = "RG12 9FG";
+
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAdd);
+		System.out.println(zipCode);
+		// TODO Auto-generated method stub
+
+	}
+
+}
